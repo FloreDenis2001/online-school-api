a3287c3eb9611d1bcaf7a3756be63f06b8c9d8d5
Denis Floare <floredenis907@yahoo.com>
2023-02-18 19:29:58 +0200

    first commit

---
+++ src/main/java/com/example/onlineschoolapi/OnlineSchoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineschoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSchoolApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineschoolapi/OnlineSchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineschoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
